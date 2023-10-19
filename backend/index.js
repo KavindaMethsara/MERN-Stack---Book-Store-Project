eadf5f1ab0726ec9a910a63174723b3d7274f90b
KavindaMethsara <methsara31@gmail.com>
2023-10-19 14:29:59 +0530

    Created a route for get one book from the database by id

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,18 @@ app.get('/books', async (request, response) => {
     }
 });
 
+//Route for get one book from the database by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
